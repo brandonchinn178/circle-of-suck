54dce6dae15f35c25a46556aa1e7fbb5726ca593
Brandon Chinn <brandonchinn178@gmail.com>
2016-11-12 15:26:14 -0800

    start working with conference view

--- circle_suck/base/views.py
+++ circle_suck/base/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
+
 from base.models import *
+from base.utils import flatten
+
 class HomeView(TemplateView):
     template_name = "home.html"
 
@@ -8,10 +11,28 @@ class ConferenceView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(ConferenceView, self).get_context_data(**kwargs)
-        context['schools'] = School.get_conference("PAC_12")
+
+        context['season'] = self.season
+        if self.season:
+            context['all_games'] = self.season.games.all()
+
+            circle_of_suck = self.season.get_circle_of_suck()
+            context['circle_of_suck'] = circle_of_suck
+
+            # all schools in a circle of suck
+            circle_schools = flatten(circle_of_suck)
+            all_schools = School.get_conference(self.season.conference)
+            # schools not in any circles of suck
+            context['extra_schools'] = set(all_schools) - set(circle_schools)
+
         return context
 
     def dispatch(self, request, *args, **kwargs):
-        self.sport = request.GET['sport']
-        self.conference = request.GET['conference']
+        sport = request.GET['sport']
+        conference = request.GET['conference']
+        year = request.GET.get('year')
+        try:
+            self.season = Season.objects.get(sport=sport, conference=conference)
+        except:
+            self.season = None
         return super(ConferenceView, self).dispatch(request, *args, **kwargs)
