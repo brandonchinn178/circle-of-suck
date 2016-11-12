6938692b4a64abd5e5741c966bcc81a2a5500c63
Allan Levy <allanwlevy@gmail.com>
2016-11-12 14:54:48 -0800

    added tests

--- circle_suck/base/utils.py
+++ circle_suck/base/utils.py
@@ -61,6 +61,37 @@ def find_all_cycles(graph, start):
         all_paths += find_all_paths(graph, starting, endindex)
     return all_paths
 
+### TEST CASES ###
+#I: {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[6,9],9:[]}
+#O: [[8, 6, 7]]
+
+#I: {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[2,6,9],9:[]}
+#O: [[2, 5, 6, 7, 8]]
+
+#I: {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[5,8],8:[6,9],9:[]}
+#O: [[8, 6, 7]] (NOTE: [[5, 6, 7]] is fine as well)
+
+#I: {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[6,9],9:[5]}
+#O: [[8, 9, 5, 6, 7]]
+
+#I: {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[2,3,6,9],9:[]}
+#O: [[3, 4, 5, 6, 7, 8]]
+
+#I: {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[2,3,6,9],9:[3]}
+#O: [[3, 4, 5, 6, 7, 8, 9]]
+
+#I: {1:[2],2:[1,5],3:[1,4],4:[3,5],5:[6],6:[7],7:[8],8:[2,3,6,9],9:[3]}
+#O: [[1, 2, 5, 6, 7, 8, 9, 3]]
+
+#I: {"Utah":["UDub","Cal"],"UDub":[],"OSU":["Utah","Wazzu","UDub","Colorado"],"Cal":["OSU","ASU", "U$C"],"Oregon":["UDub", "Wazzu", "Cal","Colorado"],"Arizona":["Utah", "UDub", "U$C", "Stanfurd","UCLA"],"ASU":["Oregon","Colorado","U$C","Wazzu"],"Colorado":["U$C"],"U$C":["Utah","Stanfurd"], "UCLA":["Utah","ASU","Wazzu","Stanfurd"], "Stanfurd":["Colorado","UDub","Wazzu"]}
+#O: [['ASU', 'Oregon', 'Colorado', 'U$C', 'Utah', 'Cal']]
+
+#I: {"Utah":["UDub"],"UDub":[],"OSU":["Utah","Wazzu","UDub","Colorado"],"Cal":["OSU","ASU", "U$C"],"Oregon":["UDub", "Wazzu", "Cal","Colorado"],"Arizona":["Utah", "UDub", "U$C", "Stanfurd","UCLA"],"ASU":["Oregon","Colorado","U$C","Wazzu"],"Colorado":["U$C"],"U$C":["Utah","Stanfurd"], "UCLA":["Utah","ASU","Wazzu","Stanfurd"], "Stanfurd":["Colorado","UDub","Wazzu"]}
+#O: [['Colorado', 'U$C', 'Stanfurd'], ['Oregon', 'Cal', 'ASU']]
+
+#I: {1:[2],2:[3],3:[4],4:[1,5,8],5:[6,7],6:[7],7:[4],8:[9],9:[4,10],10:[8]}
+#O: [[1, 2, 3, 4], [8, 9, 10]]
+
 def find_longest_cycle(graph):
     cycles = []
     for item, value in graph.items():
