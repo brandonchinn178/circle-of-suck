009d3145deb993694c261f7472f484ba22156659
Brandon Chinn <brandonchinn178@gmail.com>
2016-11-12 11:40:30 -0800

    add school model

---
+++ circle_suck/base/constants.py
@@ -0,0 +1,38 @@
+from base.utils import merge_dicts
+
+# map ID to team name. Add logo image to `img/<conference_id>/<school_id>.png` (lowercase IDs)
+PAC_12 = {
+    'CAL': 'Cal Golden Bears',
+    'UCLA': 'UCLA Bruins',
+    'COL': 'Colorado Buffaloes',
+    'ASU': 'ASU Sun Devils',
+    'USC': 'USC Trojans',
+    'UTH': 'Utah Utes',
+    'ARI': 'Arizona Wildcats',
+    'ORS': 'OSU Beavers',
+    'STA': 'Stanford Cardinal',
+    'WST': 'Washington State Cougars',
+    'ORE': 'Oregon Ducks',
+    'WAS': 'Washington Huskies',
+}
+
+# map ID to conference name and dictionary of schools
+CONFERENCES = {
+    'PAC_12': {
+        'name': 'Pac-12',
+        'schools': PAC_12,
+    },
+    # add other conferences here
+}
+
+# map ID to team name and conference ID
+SCHOOLS = merge_dicts(*[
+    {
+        school_id: {
+            'name': school,
+            'conference': conference_id,
+        }
+        for school_id, school in conference['schools'].items()
+    }
+    for conference_id, conference in CONFERENCES.items()
+])
