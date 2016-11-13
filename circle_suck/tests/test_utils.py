a4c66fa4c3beed298e5e1bcdbf2c89c8e2c18560
Thomas Zhang <thomashaozhang@gmail.com>
2016-11-12 18:02:25 -0800

    fixed tests

--- circle_suck/tests/test_utils.py
+++ circle_suck/tests/test_utils.py
@@ -30,7 +30,7 @@ class TestCycleFinder(TestCase):
 		
 		#test with real-life data from 10/30/16. Ensures string compatibility and protects from any unforseen real-world errors
 		graph = {"Utah":["UDub","Cal"],"UDub":[],"Wazzu":[],"OSU":["Utah","Wazzu","UDub","Colorado"],"Cal":["OSU","ASU", "U$C"],"Oregon":["UDub", "Wazzu", "Cal","Colorado"],"Arizona":["Utah", "UDub", "U$C", "Stanfurd","UCLA"],"ASU":["Oregon","Colorado","U$C","Wazzu"],"Colorado":["U$C"],"U$C":["Utah","Stanfurd"], "UCLA":["Utah","ASU","Wazzu","Stanfurd"], "Stanfurd":["Colorado","UDub","Wazzu"]}
-		self.assertEqual(find_all_connected(graph), [['Oregon', 'Colorado', 'U$C', 'Utah', 'Cal', 'ASU']])
+		self.assertEqual(find_all_connected(graph), [['Oregon', 'Colorado','U$C', 'Utah', 'Cal','ASU']])
 
 		#test complete circle of suck
 		graph = {"Utah":["UDub","Cal"],"UDub":["Arizona"],"Wazzu":["UDub"],"OSU":["Utah","Wazzu","UDub","Colorado"],"Cal":["OSU","ASU", "U$C"],"Oregon":["UDub", "Wazzu", "Cal","Colorado"],"Arizona":["Utah", "UDub", "U$C", "Stanfurd","UCLA"],"ASU":["Oregon","Colorado","U$C","Wazzu"],"Colorado":["U$C"],"U$C":["Utah","Stanfurd"], "UCLA":["Utah","ASU","Wazzu","Stanfurd"], "Stanfurd":["Colorado","UDub","Wazzu","OSU"]}
@@ -44,7 +44,7 @@ class TestCycleFinder(TestCase):
 		
 		#graph includes multiple fully connected components with one cycle each. Also tests real-world data.
 		graph = {"Utah":["UDub"],"UDub":[],"Wazzu":[],"OSU":["Utah","Wazzu","UDub","Colorado"],"Cal":["OSU","ASU", "U$C"],"Oregon":["UDub", "Wazzu", "Cal","Colorado"],"Arizona":["Utah", "UDub", "U$C", "Stanfurd","UCLA"],"ASU":["Oregon","Colorado","U$C","Wazzu"],"Colorado":["U$C"],"U$C":["Utah","Stanfurd"], "UCLA":["Utah","ASU","Wazzu","Stanfurd"], "Stanfurd":["Colorado","UDub","Wazzu"]}
-		self.assertEqual(find_all_connected(graph), [['Stanfurd', 'Colorado', 'U$C'], ['Oregon', 'Cal', 'ASU']])
+		self.assertEqual(find_all_connected(graph), [['Stanfurd', 'Colorado','U$C'], ['Oregon','Cal', 'ASU']])
 
 		#graph includes multiple fully connected components, one of which also has multiple disjoint cycles. Also tests for single outliers in a component (see element 15)
 		graph = {1:[2],2:[3],3:[4],4:[1,5,8],5:[6,7],6:[7],7:[4],8:[9],9:[4,10],10:[8],11:[10,12],12:[13],13:[14],14:[11,15]}
