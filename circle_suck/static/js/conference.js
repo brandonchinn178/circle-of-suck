80cce812daa31f7c417b90cac30240843c61a077
Leslie Tsai <leslietsai97@gmail.com>
2016-11-12 21:45:33 -0800

    circles of suck

--- circle_suck/static/js/conference.js
+++ circle_suck/static/js/conference.js
@@ -17,11 +17,35 @@ $(document).ready(function() {
         });
     });
 
+    $(".circle-of-suck").each(function() {
+        var n = $(this).children(".school").length;
+        var radius = 25 * n;
+        var svgSize = $("svg.school").width();
+
+        var size = 2 * radius + svgSize;
+        var center = size/2;
+        var offset = Math.PI/4; // treat 45deg as 0deg
+        var angle = 2 * Math.PI / n;
+
+        $(this).children(".school").each(function(i) {
+            var cx = center + Math.cos(angle * i + offset) * radius;
+            var cy = center + Math.sin(angle * i + offset) * radius;
+            $(this).css({
+                left: cx - svgSize/2,
+                top: cy - svgSize/2,
+            });
+        });
+
+        $(this).css({
+            width: size,
+            height: size,
+        });
+    });
+
     $("svg.school circle")
         .mouseover(function(e) {
             var id = $(this).parent().data("id");
             var data = window.allSchools[id];
-
             $(".school-box .school-name").text(data.name);
             $(".school-box .record").text("(" + data.record[0] + "-" + data.record[1] + ")");
             $(".school-box").show();
