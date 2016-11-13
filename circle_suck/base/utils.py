0ae91aca8f04666d16ddf86f538d798bcf228c55
Thomas Green <thomashaozhang@gmail.com>
2016-11-12 20:43:24 -0800

    Implemented find_longest_cycle(s) (#11)
    * Implemented find_longest_cycle(s)
    
    * Added tarjan requirement
    
    * added tests
    
    * Added tests for largest cycle
    
    * algorithm finds multiple cycles per connected component. Passes all tests!
    
    * Modified longest cycle algorithm
    
    * explained and improved tests
    
    * fixed order of final test's output
    
    * small fix
    
    * One more tiny thing
    
    * made tests look cleaner
    
    * Modified tests
    
    * fixed tests
    
    * Modified
    
    * optimization to break when longest is guaranteed
    
    * moved all test cases into a single suite to solve testing bug
    
    * removed timeit package from utils

--- circle_suck/base/utils.py
+++ circle_suck/base/utils.py
@@ -1,3 +1,5 @@
+from tarjan import *
+
 def merge_dicts(*dicts):
     """
     Merge an arbitrary amount of dictionaries together.
@@ -21,3 +23,123 @@ def flatten(l):
     [1,2,3,4,5]
     """
     return [x for sublist in l for x in sublist]
+
+def find_all_connected(graph, minsize=3):
+    """
+	Takes in a graph in the format: {footballteam1:[footballteam1 beat these footballteams]...}
+	Ex: {Cal:[Utah, Oregon]...}
+	Returns a list of all disjoint cycles in the given graph (not sorted by length)
+
+    >>> testgraph = {1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[6,9],9:[]})
+    >>> find_all_connected(testgraph)
+    [[8, 6, 7]]
+	"""
+    listoflists = []
+
+    #Use tarjan algorithm to divide into fully connected components
+    newlist = tarjan(graph)
+
+    #get rid of all connected components with fewer than minsize elements
+    for item in newlist[:]:
+        if len(item) < minsize:
+            newlist.remove(item)
+
+    #find all cycles in each fully connected component
+    while newlist:
+        con_comp = newlist[0]
+        newlist.remove(newlist[0])
+        #create graph of this connected component only
+        newgraph = {}
+        for vertex, outflow in graph.items():
+            if vertex in con_comp:
+                newgraph[vertex] = outflow
+        #find cycles in this smaller graph
+        longest_cycle = find_longest_cycle(newgraph)
+        listoflists.append(longest_cycle)
+
+        #remove cycle from graph
+        temp_dict = {};
+        for elem in con_comp:
+            if elem not in longest_cycle:
+                temp_dict[elem] = newgraph[elem]
+        newgraph = temp_dict
+
+        #re-apply tarjan with smaller graph
+        subgraphs = tarjan(newgraph)
+
+        #remove all connected components with fewer than minsize elements, and rerun algorithm on the rest
+        for subgraph in subgraphs:
+            if len(subgraph) < minsize:
+                subgraphs.remove(subgraph)
+            else:
+                newlist.append(subgraph)
+    return listoflists
+
+def find_longest_cycle(graph):
+    cycles = []
+    items_left = len(graph)
+    max_cycle_len = 0
+    for item, value in graph.items():
+        new_cycles = find_all_cycles(graph, item)
+        for c in new_cycles:
+            if len(c) > max_cycle_len:
+                max_cycle_len = len(c)
+        cycles += new_cycles
+        items_left -= 1
+
+        #Once we have a cycle larger than the amount of elements 
+            #we are yet to try starting the algorithm at, we must 
+            #have the longest cycle in the graph. Any newly discovered
+            #cycles would only exist in the elements not yet checked,
+            #and we already know there are fewer of those than there
+            #are elements in our longest cycle so far.
+        if max_cycle_len > items_left:
+            break
+    return max(cycles, key = lambda x: len(x))
+
+def find_all_cycles(graph, start):
+    def find_all_paths(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if not start in graph.keys():
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+    endingindexes = [k for k, v in graph.items() if start in v]
+
+    all_paths = []
+    for endindex in endingindexes:
+        all_paths += find_all_paths(graph, start, endindex)
+    return all_paths
+
+def same_cycle(cycle1, cycle2):
+    """
+    checks if 2 cycles are the actually the same
+    :param cycle1:
+    :param cycle2:
+    :return true or false:
+
+    >>> cycle1 = [1, 2, 3]
+    >>> cycle2 = [2, 3, 1]
+    >>> same_cycle(cycle1, cycle2)
+    True
+    """
+    offset = 0
+    if len(cycle1) != len(cycle2):
+        return False
+    if cycle1 and cycle2:
+        for c in cycle2:
+            if c == cycle1[0]:
+                break
+            offset += 1
+    for i in range(len(cycle1)):
+        if cycle1[i] != cycle2[(i + offset) % len(cycle2)]:
+            return False
+    return True
