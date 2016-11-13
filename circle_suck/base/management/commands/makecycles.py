2807f6c1e97c20ac02a3f8bf8d2bbe75a5600d67
Brandon Chinn <brandonchinn178@gmail.com>
2016-11-13 04:38:23 -0800

    fix makecycles

--- circle_suck/base/management/commands/makecycles.py
+++ circle_suck/base/management/commands/makecycles.py
@@ -18,11 +18,12 @@ class Command(BaseCommand):
     
     def update_circles(self, sport, sport_url, **options):
         for conference_id, conference in CONFERENCES.items():
-            print '    - %s...' % conference.name
+            print '    - %s...' % conference['name']
             season = Season.objects.get(sport = sport, conference = conference_id, year = self.year)
             games = Game.objects.all().filter(season = season)
             graph = {
-                school.id: games.filter(loser=school.id).values_list('winner', flat=True) for school in conference['schools']
+                school.id: games.filter(loser=school.id).values_list('winner', flat=True)
+                for school in conference['schools']
             }
             circles = utils.find_cycles(graph)
             season.set_circle_of_suck(circles)
