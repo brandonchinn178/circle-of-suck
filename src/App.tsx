56eb55b9a57effc17fa9074004869360ed683818
Brandon Chinn <brandonchinn178@gmail.com>
2019-11-04 00:24:15 -0800

    Show year

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import React, { FC } from 'react'
 import { CircleOfSuck } from './CircleOfSuck'
 
 export const App: FC = () => {
+  // TODO: make inputtable by user
+  const date = new Date()
+  // year should initially be the year of the last fall season
+  const year = date.getFullYear() - (date.getMonth() < 6 ? -1 : 0)
+
   return (
     <main>
-      <h1>PAC-12 Circle of Suck</h1>
+      <h1>PAC-12 Circle of Suck ({year})</h1>
       <CircleOfSuck year={2019} conference="PAC" />
     </main>
   )
