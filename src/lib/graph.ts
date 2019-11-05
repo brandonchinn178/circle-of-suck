b4efa68f3e1afd0b99c175df0e04716f94e08bdf
Brandon Chinn <brandon@leapyear.io>
2019-11-04 20:50:49 -0800

    Implement shortest hamiltonian path

--- src/lib/graph.ts
+++ src/lib/graph.ts
@@ -1,41 +1,63 @@
-import jsgraphs from 'js-graph-algorithms'
 import _ from 'lodash'
 
-/**
- * Get the hamiltonian path of the given graph, if one exists.
- */
-export const getHamiltonian = (graph: jsgraphs.DiGraph): number[] | null => {
-  const cc = new jsgraphs.StronglyConnectedComponents(graph)
+export class WeightedDiGraph {
+  size: number
+  adjMatrix: (number | null)[][]
 
-  if (cc.componentCount() !== 1 || !_.every(_.range(graph.V), (v) => cc.componentId(v) === 0)) {
-    return null
+  constructor(size: number) {
+    this.size = size
+    this.adjMatrix = _.map(Array(size), () => _.fill(Array(size), null))
   }
 
-  return new jsgraphs.TopologicalSort(graph).order()
+  addEdge(n1: number, n2: number, weight: number) {
+    if (n1 >= this.size || n2 >= this.size) {
+      throw new Error('Vertex does not exist')
+    }
+    this.adjMatrix[n1][n2] = weight
+  }
+
+  allFrom(n: number): { weight: number; neighbor: number }[] {
+    if (n >= this.size) {
+      throw new Error('Vertex does not exist')
+    }
+    return _.compact(this.adjMatrix[n].map((weight, neighbor) =>
+      weight === null ? null : { weight, neighbor }
+    ))
+  }
+}
+
+type Path = {
+  nodes: number[]
+  weight: number
 }
 
 /**
- * Get the longest path in the given graph.
+ * Get the shortest hamiltonian path of the given graph.
  */
-export const getLongestPath = (graph: jsgraphs.DiGraph): number[] => {
-  const longestPathFrom = (
-    curr: number,
-    seen: number[], // has 'curr' already
-  ): number[] => {
-    const neighborPaths = _.compact(_.map(graph.adj(curr), (neighbor) => {
-      if (_.includes(seen, neighbor)) {
-        return null
+export const getHamiltonian = (graph: WeightedDiGraph): number[] | null => {
+  const START_NODE = 0
+
+  const getHamiltonianFrom = (curr: number, currPath: Path): Path[] => {
+    return _.flatMap(graph.allFrom(curr), ({ neighbor, weight }) => {
+      if (neighbor === START_NODE && currPath.nodes.length === graph.size) {
+        return [currPath]
       }
 
-      return longestPathFrom(neighbor, _.concat(seen, neighbor))
-    }))
+      if (_.includes(currPath.nodes, neighbor)) {
+        return []
+      }
 
-    return _.concat([curr], getLongest(neighborPaths))
+      return getHamiltonianFrom(neighbor, {
+        nodes: _.concat(currPath.nodes, neighbor),
+        weight: currPath.weight + weight,
+      })
+    })
   }
 
-  const vertexPaths = _.map(_.range(graph.V), (vertex) => longestPathFrom(vertex, [vertex]))
-  return getLongest(vertexPaths)
+  const paths = getHamiltonianFrom(START_NODE, { nodes: [START_NODE], weight: 0 })
+  const shortestPath = _.minBy(paths, 'weight')
+  if (!shortestPath) {
+    return null
+  }
+  return shortestPath.nodes
 }
-
-// Get longest list in the given list of lists. Returns empty list if an empty list is provided.
-const getLongest = <T>(list: T[][]): T[] => _.orderBy(list, [_.size], ['desc'])[0] || []
