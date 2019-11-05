59407053023dbd7e52014dffe967c55c740cea0d
Brandon Chinn <brandonchinn178@gmail.com>
2019-11-04 22:34:49 -0800

    Separate finding circle of suck as hook from rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useState } from 'react'
+import Graph from 'react-graph-vis'
 
-import { CircleOfSuck } from './CircleOfSuck'
+import { Conference } from './lib/api'
+import { useCircleOfSuck } from './CircleOfSuck'
 
 // year should initially be the year of the last fall season
 const NOW = new Date()
@@ -13,7 +15,45 @@ export const App: FC = () => {
   return (
     <main>
       <h1>PAC-12 Circle of Suck ({year})</h1>
-      <CircleOfSuck year={2019} conference="PAC" />
+      <CircleOfSuck year={year} conference="PAC" />
     </main>
+  )
+}
+
+const CircleOfSuck: FC<{ year: number; conference: Conference }> = ({ year, conference }) => {
+  const { loading, circleOfSuck, teams } = useCircleOfSuck(year, conference)
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (circleOfSuck === null) {
+    // TODO: show some other interesting graph
+    return <p>No possible circle of suck for this season.</p>
+  }
+
+  return (
+    <Graph
+      graph={{
+        nodes: teams!.map(({ school, abbreviation }) => ({
+          id: school,
+          label: `${school} (${abbreviation})`,
+        })),
+        edges: circleOfSuck.map(({ from, to, isPlayed }) => {
+          return {
+            from: from.school,
+            to: to.school,
+            width: isPlayed ? 2 : 1,
+            dashes: !isPlayed,
+          }
+        })
+      }}
+      options={{
+        height: '600px',
+        physics: {
+          enabled: false,
+        },
+      }}
+    />
   )
 }
