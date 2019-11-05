57803a243aed2a6ea7a97d4d578e2fb3fe5b8f11
Brandon Chinn <brandon@leapyear.io>
2019-11-04 22:35:47 -0800

    Reindent

--- src/CircleOfSuck.ts
+++ src/CircleOfSuck.ts
@@ -43,51 +43,51 @@ type CircleOfSuckEdge = {
 }
 
 const findCircleOfSuck = async (teams: Team[], games: Game[]): Promise<CircleOfSuckEdge[] | null> => {
-    // maps winner team -> loser team
-    const gameGraph = _.fromPairs(_.map(teams, ({ school }) => [school, [] as string[]]))
+  // maps winner team -> loser team
+  const gameGraph = _.fromPairs(_.map(teams, ({ school }) => [school, [] as string[]]))
 
-    // contains all future games, as [home_team, away_team] pairs
-    const futureGames = [] as [string, string][]
+  // contains all future games, as [home_team, away_team] pairs
+  const futureGames = [] as [string, string][]
 
-    _.each(games, ({ conference_game, away_team, home_team, away_points, home_points }) => {
-      if (!conference_game) {
-        return
-      }
-
-      if (away_points === null || home_points === null) {
-        futureGames.push([home_team, away_team])
-      } else if (away_points > home_points) {
-        gameGraph[away_team].push(home_team)
-      } else {
-        gameGraph[home_team].push(away_team)
-      }
-    })
-
-    const teamToIndex = _.fromPairs(_.map(teams, ({ school }, i) => [school, i]))
-
-    const graph = new WeightedDiGraph(teams.length)
-    _.each(gameGraph, (losers, winner) => {
-      _.each(losers, (loser) => {
-        graph.addEdge(teamToIndex[winner], teamToIndex[loser], 0)
-      })
-    })
-    _.each(futureGames, ([home, away]) => {
-      graph.addEdge(teamToIndex[home], teamToIndex[away], 1)
-      graph.addEdge(teamToIndex[away], teamToIndex[home], 1)
-    })
-
-    const hamiltonian = getHamiltonian(graph)
-    if (!hamiltonian) {
-      return null
+  _.each(games, ({ conference_game, away_team, home_team, away_points, home_points }) => {
+    if (!conference_game) {
+      return
     }
 
-    return _.map(hamiltonian.map((v) => teams[v]), (team1, i, arr) => {
-      const team2 = arr[i === arr.length - 1 ? 0 : i + 1]
+    if (away_points === null || home_points === null) {
+      futureGames.push([home_team, away_team])
+    } else if (away_points > home_points) {
+      gameGraph[away_team].push(home_team)
+    } else {
+      gameGraph[home_team].push(away_team)
+    }
+  })
 
-      return {
-        from: team1,
-        to: team2,
-        isPlayed: _.includes(gameGraph[team1.school], team2.school),
-      }
+  const teamToIndex = _.fromPairs(_.map(teams, ({ school }, i) => [school, i]))
+
+  const graph = new WeightedDiGraph(teams.length)
+  _.each(gameGraph, (losers, winner) => {
+    _.each(losers, (loser) => {
+      graph.addEdge(teamToIndex[winner], teamToIndex[loser], 0)
     })
+  })
+  _.each(futureGames, ([home, away]) => {
+    graph.addEdge(teamToIndex[home], teamToIndex[away], 1)
+    graph.addEdge(teamToIndex[away], teamToIndex[home], 1)
+  })
+
+  const hamiltonian = getHamiltonian(graph)
+  if (!hamiltonian) {
+    return null
   }
+
+  return _.map(hamiltonian.map((v) => teams[v]), (team1, i, arr) => {
+    const team2 = arr[i === arr.length - 1 ? 0 : i + 1]
+
+    return {
+      from: team1,
+      to: team2,
+      isPlayed: _.includes(gameGraph[team1.school], team2.school),
+    }
+  })
+}
